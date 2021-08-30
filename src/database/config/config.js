d780a844cdb6ed5684f96ee978388b1d5a374ab2
Hiep Nguyen <hiepnh610@gmail.com>
2021-08-30 20:09:42 +0700

    feat: update job config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,3 @@
-console.log('POSTGRES_USER', process.env.POSTGRES_USER);
-console.log('POSTGRES_PASSWORD', process.env.POSTGRES_PASSWORD);
-console.log('POSTGRES_SCHEMA', process.env.POSTGRES_SCHEMA);
-console.log('POSTGRES_HOST', process.env.POSTGRES_HOST);
-
 module.exports = {
   development: {
     username: process.env.POSTGRES_USER,
